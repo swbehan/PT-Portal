b1ccdb635198ed0bdc5118aef9757e2bf60b56da
Sean Behan <behan.s@northeastern.edu>
2026-06-13 22:28:27 -0400

    finishing about page

---
+++ frontend/js/animation.js
@@ -0,0 +1,25 @@
+export default function Animation() {
+    const me = {};
+  
+    const textReveal = (classToBeAdded, classOfElement) => {
+      const windowInnerHeight = window.innerHeight;
+      const element = document.querySelector(classOfElement);
+  
+      if (element.getBoundingClientRect().top <= windowInnerHeight) {
+        if (!element.classList.contains(classToBeAdded)) {
+          element.classList.add(classToBeAdded);
+          console.log('Scrolled into view');
+        }
+      }
+    };
+  
+    me.handleScroll = (classToBeAdded, classOfElement) => {
+      textReveal(classToBeAdded, classOfElement);
+      const element = document.querySelector(classOfElement);
+      if (element.classList.contains(classToBeAdded)) {
+        document.removeEventListener('scroll', me.handleScroll);
+      }
+    };
+  
+    return me;
+  }
--- frontend/js/sign-in.js
+++ frontend/js/sign-in.js
@@ -1,36 +1,19 @@
-function SignInMain() {
-  const me = {};
-
-  const textReveal = (classToBeAdded, classOfElement) => {
-    const windowInnerHeight = window.innerHeight;
-    const element = document.querySelector(classOfElement);
-
-    if (element.getBoundingClientRect().top <= windowInnerHeight) {
-      if (!element.classList.contains(classToBeAdded)) {
-        element.classList.add(classToBeAdded);
-        console.log('Scrolled into view');
-      }
-    }
-  };
-
-  me.handleScroll = (classToBeAdded, classOfElement) => {
-    textReveal(classToBeAdded, classOfElement);
-    const element = document.querySelector(classOfElement);
-    if (element.classList.contains(classToBeAdded)) {
-      document.removeEventListener('scroll', me.handleScroll);
-    }
-  };
-
-  return me;
-}
+import Animation from "./animation.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-  const signInMain = SignInMain();
+  const animationMain = Animation();
 
   document.addEventListener('scroll', () =>
-    signInMain.handleScroll('visible', '.why-us'),
+    animationMain.handleScroll('visible', '.why-us'),
   );
   document.addEventListener('scroll', () =>
-    signInMain.handleScroll('visible', '.sign-in'),
+    animationMain.handleScroll('visible', '.sign-in'),
+  );
+  document.addEventListener('scroll', () =>
+    animationMain.handleScroll('visible', '.pt-portal-platform'),
+  );
+  document.addEventListener('scroll', () =>
+    animationMain.handleScroll('visible', '.what-is-pt-portal'),
   );
 });
+ 
